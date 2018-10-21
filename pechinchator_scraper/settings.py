03cf70a14639a9b84c64309214991bdb10b4f315
Leonardo Couy <contato@leonardocouy.com>
2018-10-21 20:53:30 -0300

    Set HTTPCACHE_ENABLED setting to True
    This commits adds a setting that avoid make requests repeatedly

--- pechinchator_scraper/settings.py
+++ pechinchator_scraper/settings.py
@@ -84,7 +84,7 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
+HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
