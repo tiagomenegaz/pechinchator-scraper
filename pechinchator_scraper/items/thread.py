516ba594216a84d6ffd5d2b98188e80214dcd8b5
Leonardo Couy <contato@leonardocouy.com>
2018-10-25 21:26:25 -0300

    Add content_html field to ThreadItem

--- pechinchator_scraper/items/thread.py
+++ pechinchator_scraper/items/thread.py
@@ -5,6 +5,7 @@ class Thread(scrapy.Item):
     url = scrapy.Field()
     title = scrapy.Field()
     posted_at = scrapy.Field()
+    content_html = scrapy.Field()
     replies_count = scrapy.Field()
     visits_count = scrapy.Field()
     thread_id = scrapy.Field()
