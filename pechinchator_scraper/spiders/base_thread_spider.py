6575a40495af0c8ab0d9c26c124c37e326d5023f
Leonardo Couy <contato@leonardocouy.com>
2018-10-31 20:16:55 -0300

    Add a strategy to cache when necessary
    This commit add a strategy to cache when necessary, the strategy is:
    
    - Don't cache the main page (start_url), most of the time start_url is the thread list page;
    - Cache only the thread detail page, because we need access the page a once to extract the content and posted_at date;

--- pechinchator_scraper/spiders/base_thread_spider.py
+++ pechinchator_scraper/spiders/base_thread_spider.py
@@ -5,6 +5,11 @@ class BaseThreadSpider(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def start_requests(self):
+        for url in self.start_urls:
+            meta = {"dont_cache": True}
+            yield scrapy.Request(url=url, callback=self.parse, meta=meta)
+
     def parse(self, response):
         raise NotImplementedError
 
