de9660767d582848f2159845db108e803db6ad06
Leonardo Couy <contato@leonardocouy.com>
2018-11-02 21:12:56 -0300

    Add schedule job to run spiders every 5 minutes

---
+++ scheduled_tasks.py
@@ -0,0 +1,27 @@
+import multiprocessing
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from pechinchator_scraper.spiders.hardmob_spider import HardmobSpider
+from pechinchator_scraper.spiders.adrenaline_spider import AdrenalineSpider
+
+scheduler = BlockingScheduler()
+
+
+def crawl():
+    spider_runner = CrawlerProcess(get_project_settings())
+    spider_runner.crawl(HardmobSpider)
+    spider_runner.crawl(AdrenalineSpider)
+    spider_runner.start()
+
+
+@scheduler.scheduled_job("interval", minutes=5)
+def run():
+    process = multiprocessing.Process(target=crawl)
+    process.start()
+
+
+if __name__ == '__main__':
+    scheduler.start()
