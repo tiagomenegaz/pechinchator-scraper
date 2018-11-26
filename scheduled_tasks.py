96b82c564506fb45e6ae36d3973f558ac167faa6
Leonardo Couy <contato@leonardocouy.com>
2018-11-26 07:31:06 -0200

    Add Kabum spider to scheduled tasks

--- scheduled_tasks.py
+++ scheduled_tasks.py
@@ -7,9 +7,14 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from pechinchator_scraper.spiders.hardmob_spider import HardmobSpider
 from pechinchator_scraper.spiders.adrenaline_spider import AdrenalineSpider
 from pechinchator_scraper.spiders.promobit_spider import PromobitSpider
+from pechinchator_scraper.spiders.kabum_spider import KabumSpider
 
 scheduler = BlockingScheduler()
 
+def kabum_crawl():
+    spider_runner = CrawlerProcess(get_project_settings())
+    spider_runner.crawl(KabumSpider)
+    spider_runner.start()
 
 def crawl():
     spider_runner = CrawlerProcess(get_project_settings())
@@ -24,6 +29,11 @@ def run():
     process = multiprocessing.Process(target=crawl)
     process.start()
 
+
+@scheduler.scheduled_job("interval", minutes=3)
+def run():
+    process = multiprocessing.Process(target=kabum_crawl)
+    process.start()
 
 if __name__ == '__main__':
     scheduler.start()
